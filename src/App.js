224dfef914a1b44c9e837f2417080457f9f6afb9
Sayyed-Abrar-Akhtar <58516708+Sayyed-Abrar-Akhtar@users.noreply.github.com>
2022-06-10 17:46:56 +0545

    Developed and build monster-rolodex-react app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      string: ' Sayyed',
+      monsters: [],
+      searchField: '',
     };
+
+    //this.handleChange = this.handleChange.bind(this);
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  // handleChange(e) {
+  //   this.setState({ searchField: e.target.value });
+  // }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Salaam, {this.state.string}
-          </a>
-          <button onClick={() => this.setState({ string: 'Sayyed Abrar' })}>
-            Change Text
-          </button>
-        </header>
+        <h1 className='title'>monsters Rolodex</h1>
+        <SearchBox placeholder='search...' handleChange={this.handleChange} />
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
